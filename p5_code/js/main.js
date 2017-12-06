21398894c0d1c8b65ee115914fee6e578de9cc48
camillapc <31709814+camillapc@users.noreply.github.com>
2017-12-06 11:55:02 -0500

    added credits

--- p5_code/js/main.js
+++ p5_code/js/main.js
@@ -1,5 +1,5 @@
 /*
-Test Gui
+Great Heart - A Heartbeat Meditation Device by Jim Schmitz and Camilla Padgitt-Coles - ITP-NYU 2017
 */
 class CircleVisualization1 {
   constructor() {
@@ -159,7 +159,7 @@ function setup() {
   imageMode(CENTER);
 
   // serial connection setup
-  serial = new p5.SerialPort(); // make a new instance of the serialport library
+  serial = new p5.SerialPort("172.16.229.224"); // make a new instance of the serialport library
   serial.on('data', serialEvent); // callback for when new data arrives
   serial.on('error', serialError); // callback for errors
   serial.open(portName); // open a serial port
